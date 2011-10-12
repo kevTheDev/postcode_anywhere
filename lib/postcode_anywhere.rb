a880093b3f5fa919d5ff6e6270517d904d6ac255
Kevin Edwards <kev.j.edwards@gmail.com>
2011-10-12 15:38:17 +0100

    refactoring

--- lib/postcode_anywhere.rb
+++ lib/postcode_anywhere.rb
@@ -8,32 +8,68 @@ module PostcodeAnywhere
   
   class << self
     
-    attr_accessor :key
+    attr_accessor :key, :postcode, :number
     
     def license_key
       @key
     end
+    
+    
   end
   
-  def self.find_by_number_and_postcode(number, postcode)
+  def self.lookup(query_options={})
     PostcodeAnywhere.validate_key
-    sanitised_postcode = postcode.gsub(/\s/, "")
-    data = PostcodeAnywhere.lookup(number, sanitised_postcode)
+    data = PostcodeAnywhere.lookup(postcode)
+    data["Table"]["Row"]
+  end
+  
+  def self.find_by_postcode(postcode)
+    PostcodeAnywhere.validate_key
+    data = PostcodeAnywhere.lookup(postcode)
+    data["Table"]["Row"]
+  end
+  
+  def self.find_by_postcode_and_number(postcode, number)
+    PostcodeAnywhere.validate_key
+    data = PostcodeAnywhere.lookup(postcode, number)
     data["Table"]["Row"]
   end
   
   protected
+  
+  def params
+    {
+      :Key      => self.key,
+      :Postcode => self.postcode,
+      :Building => self.number
+    }
+  end
+  
+  def query_string
+    params.map{|k,v| "#{CGI::escape(k.to_s)}=#{CGI::escape(v)}"}.join('&')
+  end
+  
+  # appends query string to HOST_URL
+  def to_s
+    "#{HOST_URL}?#{query_string}"  
+  end
+  
   
   def self.validate_key
     unless PostcodeAnywhere.key
       raise PostcodeAnywhereException, "Please provide a valid Postcode Anywhere License Key"
     end
   end
-
-  def self.lookup(number, postcode)
-    PostcodeAnywhere.get SERVICE_ADDRESS+"?Key=#{PostcodeAnywhere.key}&Postcode=#{postcode}&Building=#{number}"
+  
+  def self.sanitised_postcode(postcode)
+    postcode.gsub(/\s/, "")
   end
   
+  def self.lookup(postcode, number='')
+    #PostcodeAnywhere.get SERVICE_ADDRESS+"?Key=#{PostcodeAnywhere.key}&Postcode=#{sanitised_postcode(postcode)}&Building=#{number}"
+    PostcodeAnywhere.get "#{SERVICE_ADDRESS}?#{query_string}"
+  end
+
   class PostcodeAnywhereException < StandardError;end
   
 end
