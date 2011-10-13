98168a0070f17d6c22d218580a1b11481eaa9fe4
Kevin Edwards <kev.j.edwards@gmail.com>
2011-10-13 13:02:25 +0100

    made the postcode params more flexible

--- lib/hatch/postcode_anywhere.rb
+++ lib/hatch/postcode_anywhere.rb
@@ -15,11 +15,12 @@ module Hatch
       data["Table"]["Row"]
     end
     
-    # TODO - we should be able to accept common aliases for postcode var
     def self.params(options={})
+      postal_code = sanitised_postcode( find_postal_code(options) )
+      
       params_hash = {
         :Key      => POSTCODE_ANYWHERE_KEY,
-        :Postcode => sanitised_postcode(options[:postcode]),
+        :Postcode => postal_code,
         :Building => options[:number]
       }
       params_hash.delete_if {|k,v| v.nil? || v.empty? }
@@ -29,6 +30,8 @@ module Hatch
       params(options).map{|k,v| "#{CGI::escape(k.to_s)}=#{CGI::escape(v)}"}.join('&')
     end
 
+    private
+
     def self.validate_key
       unless POSTCODE_ANYWHERE_KEY
         raise PostcodeAnywhereException, "Please provide a valid Postcode Anywhere License Key"
@@ -36,7 +39,24 @@ module Hatch
     end
     
     def self.sanitised_postcode(postcode)
-      postcode.gsub(/\s/, "")
+      postcode.nil? ? nil : postcode.gsub(/\s/, "")
+    end
+    
+    def self.find_postal_code(options={})
+      case
+      when options.has_key?(:postal_code)
+        options[:postal_code]
+      when options.has_key?(:postcode)
+        options[:postcode]
+      when options.has_key?(:post_code)
+        options[:post_code]
+      when options.has_key?(:zip_code)
+        options[:zip_code]
+      when options.has_key?(:zipcode)
+        options[:zipcode]
+      else
+        nil
+      end
     end
 
     class PostcodeAnywhereException < StandardError;end
